44a9c9d5f6e669c55625ab840c73049fb8eeda6b
pengda <1295831067@qq.com>
2019-07-05 11:40:49 +0800

    实现zuul网关

--- nacos-springCloud-producer/src/main/java/com/nacos/code/controller/controller.java
+++ nacos-springCloud-producer/src/main/java/com/nacos/code/controller/controller.java
@@ -4,12 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api")
 public class controller {
     @Value("${server.port}")
     String port;
+    @GetMapping(value = "/info")
+    public String baidu(@RequestParam String username){
+        return "zuul测试";
+    }
 
 
 }
--- nacos-zuul/src/main/java/com/nacos/code/ZuulApplication.java
+++ nacos-zuul/src/main/java/com/nacos/code/ZuulApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * zuul网关
@@ -16,4 +17,5 @@ public class ZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
     }
+
 }
